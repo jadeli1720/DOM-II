3067076a91ee65a9053ccfe25b5fc551a3264751
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-06-18 13:01:52 -0600

    WIP: Started less compiler and started on index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,39 @@
-// Your code goes here
+
+//******** Mouseover Events:
+const header = document.querySelector('.main-navigation')
+const navs = document.querySelector('.nav-link')
+
+
+// Mouse Enter/Hover over events
+header.addEventListener('mouseenter',(event) => {
+    console.log(`Header changed color`)
+    header.style.background = '	mediumturquoise'
+})
+
+navs.addEventListener('mouseover',(event) => {
+    console.log(`Navigation changed color`)
+    navs.style.color = 'white'
+})
+
+//Mouse leave/out events 
+header.addEventListener('mouseleave',(event) => {
+    console.log(`Header changed color`)
+    header.style.background = 'white'
+})
+
+navs.addEventListener('mouseout',(event) => {
+    console.log(`Navigation changed color`)
+    navs.style.color = 'black'
+})
+
+
+//******** Keydown Events:
+
+
+//******** Wheel Events:
+let reSize = document.querySelector('body');
+
+reSize.addEventListener('wheel', (event) => {
+    reSize.style.background = 'papayawhip'
+    event.preventDefault()
+})
