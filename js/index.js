6442aa7b48e92af500474bd24dd4536ea225aab6
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-07-09 15:18:17 -0600

    Refactored and fixed navigation problem, however, all event are occuring with 'onclick' listener only and I don't know why

--- js/index.js
+++ js/index.js
@@ -1,22 +1,41 @@
-// debug box:
-//way to find values of html elements by using the rapid-fire mouse event to locate them.
-function showMouseEvent(event) {
-    const debugClientX = document.querySelector('.clientX')
-    const debugClientY = document.querySelector('.clientY')
-    const debugTarget = document.querySelector('.target')
-    debugClientX.textContent = event.clientX
-    debugClientY.textContent = event.clientY
-    debugTarget.textContent = `${event.target.tagName} #${event.target.id}`
-  }
-  
-  document.body.addEventListener('mousemove', event => {
-    // console.log('mousemove', event)
-    showMouseEvent(event)//This allows for the debug box to work
-  if (event.target.tagName === 'IMG') {
-      const {clientX, clientY, x, y, target} = event
-      console.log(clientX, clientY, x, y, target.tagName)
-      
-    }  
-  })
+
 
 // Your code goes here
+//******** Mouseover/out:
+let navLinks = document.querySelectorAll('.nav-link')
+
+// console.log(navLinks, 'navLinks')
+
+navLinks.forEach(a => {
+  // console.log(a)
+  a.addEventListener('mouseover', event => {
+    console.log(`Navigation changed to white`)
+    event.target.style.color = 'white'
+
+    setTimeout( () => {
+      event.target.style.color ="";
+    }, 800)
+  })
+})
+
+// navLinks.forEach(a => {
+//   // console.log(a)
+//   a.addEventListener('mouseout', event => {
+//     console.log(`Navigation changed black`)
+//     event.target.style.color = '#212529'
+//   })
+// })
+
+
+// //******** Mouseenter/leave: 
+const header = document.querySelector('.main-navigation')
+
+header.addEventListener('mouseenter',(event) => {
+    console.log(`Header changed to turquoise`)
+    header.style.background = '	mediumturquoise'
+})
+
+header.addEventListener('mouseleave',(event) => {
+    console.log(`Header changed to white`)
+    header.style.background = 'white'
+})
