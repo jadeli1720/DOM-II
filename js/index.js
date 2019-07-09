1f894ad66276880a311a0ab28265c98e6cf79124
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2019-07-09 13:58:55 -0600

    Added debug box (and corresponding .less file) to help with creating event listeners.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,22 @@
+// debug box:
+//way to find values of html elements by using the rapid-fire mouse event to locate them.
+function showMouseEvent(event) {
+    const debugClientX = document.querySelector('.clientX')
+    const debugClientY = document.querySelector('.clientY')
+    const debugTarget = document.querySelector('.target')
+    debugClientX.textContent = event.clientX
+    debugClientY.textContent = event.clientY
+    debugTarget.textContent = `${event.target.tagName} #${event.target.id}`
+  }
+  
+  document.body.addEventListener('mousemove', event => {
+    // console.log('mousemove', event)
+    showMouseEvent(event)//This allows for the debug box to work
+  if (event.target.tagName === 'IMG') {
+      const {clientX, clientY, x, y, target} = event
+      console.log(clientX, clientY, x, y, target.tagName)
+      
+    }  
+  })
+
 // Your code goes here
